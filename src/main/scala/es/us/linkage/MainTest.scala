e19143a177d6678c54b309f7de9649aa59e71ec0
José María Luna <josemarialunaromera@gmail.com>
2016-11-23 13:43:20 +0100

    Fixed minor bugs and added new new distance strategy

--- src/main/scala/es/us/linkage/MainTest.scala
+++ src/main/scala/es/us/linkage/MainTest.scala
@@ -16,11 +16,23 @@ object MainTest {
     val fileOriginal = "C:\\datasets\\trabajadores.csv"
     val fileOriginalMin = "C:\\datasets\\trabajadores-min.csv"
 
-    val distances = sc.parallelize(Seq(Vectors.dense(0.0, 7.0, 2.0, 3.0), Vectors.dense(7.0, 0.0, 1.0, 4.0), Vectors.dense(2.0, 1.0, 0.0, 5.0), Vectors.dense(3.0, 4.0, 5.0, 0.0)))
-    val data = sc.parallelize(Seq(new Cluster(List(0)), new Cluster(List(1)), new Cluster(List(2)), new Cluster(List(3))))
+    val distances = sc.parallelize(Seq(
+      Vectors.dense(0.0, 1.0, 3.0, 5.0, 6.0),
+      Vectors.dense(1.0, 0.0, 3.0, 7.0, 8.0),
+      Vectors.dense(3.0, 3.0, 0.0, 7.0, 8.0),
+      Vectors.dense(5.0, 7.0, 7.0, 0.0, 2.0),
+      Vectors.dense(6.0, 8.0, 8.0, 2.0, 0.0)))
 
-    val linkage = new Linkage(1, "Min")
+    val data = sc.parallelize(Seq(
+      new Cluster(List(0)),
+      new Cluster(List(1)),
+      new Cluster(List(2)),
+      new Cluster(List(3)),
+      new Cluster(List(4))))
 
+    //min,max,avg
+    val linkage = new Linkage(4, "max")
+    
     //println(linkage.prueba(new Cluster(List(3, 2)), new Cluster(List(1)), distances.zipWithIndex().map(_.swap).collectAsMap))
 
     val model = linkage.runAlgorithm(data, distances.zipWithIndex().map(_.swap).collectAsMap())
