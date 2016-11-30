0e5dd750a5134685571af109473b99043d1a3d62
José María Luna <josemarialunaromera@gmail.com>
2016-11-30 16:59:31 +0100

    Fixed output Parameters added

--- src/main/scala/es/us/linkage/MainReadFolder.scala
+++ src/main/scala/es/us/linkage/MainReadFolder.scala
@@ -1,6 +1,5 @@
 package es.us.linkage
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object MainReadFolder {
@@ -11,39 +10,43 @@ object MainReadFolder {
       .setMaster("local[*]")
 
     val sc = new SparkContext(conf)
-    //val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val fileOriginal = "C:\\datasets\\distances"
 
-    val fileOriginal = "C:\\datasets\\trabajadores.csv"
-    val fileOriginalMin = "C:\\datasets\\trabajadores-min.csv"
-    val dirDistances = "C:\\datasets\\distances"
+    var origen: String = fileOriginal
+    var destino: String = Utils.whatTimeIsIt()
+    var numPartitions = 4 // cluster has 25 nodes with 4 cores. You therefore need 4 x 25 = 100 partitions.
+    var numPoints = 5
+    var numClusters = 4
+    var strategyDistance = "min"
+
+    if (args.size > 2) {
+      origen = args(0)
+      destino = args(1)
+      numPartitions = args(2).toInt
+      numPoints = args(3).toInt
+      numClusters = args(4).toInt
+      strategyDistance = args(5)
+    }
+
+    val distances = sc.textFile(origen, numPartitions).map(_.toDouble)
+
+    println(distances.count())
+
+    //val distances: RDD[Double] = sc.parallelize(Seq(
+    //(0.0), (1.0), (3.0), (5.0), (6.0), (1.0), (0.0), (3.0), (7.0), (8.0), (3.0), (3.0), (0.0), (7.0), (8.0), (5.0), (7.0), (7.0), (0.0), (2.0), (6.0), (8.0), (8.0), (2.0), (0.0)) )
 
 
-    //val distances = sc.textFile(dirDistances).map(_.toDouble)
-
-    //println(distances.count())
-
-    val distances: RDD[Double] = sc.parallelize(Seq(
-      (0.0), (1.0), (3.0), (5.0), (6.0), (1.0), (0.0), (3.0), (7.0), (8.0), (3.0), (3.0), (0.0), (7.0), (8.0), (5.0), (7.0), (7.0), (0.0), (2.0), (6.0), (8.0), (8.0), (2.0), (0.0)))
-
-    val data = sc.parallelize(Seq(
-      new Cluster(List(0)),
-      new Cluster(List(1)),
-      new Cluster(List(2)),
-      new Cluster(List(3)),
-      new Cluster(List(4))))
+    val data = sc.parallelize(Cluster.createInitClusters(numPoints), numPartitions)
 
     //min,max,avg
-    val linkage = new Linkage(4, "min")
-
-    //println(linkage.prueba(new Cluster(List(3, 2)), new Cluster(List(1)), distances.zipWithIndex().map(_.swap).collectAsMap))
+    val linkage = new Linkage(numClusters,strategyDistance)
 
     val model = linkage.runAlgorithm(data, distances.zipWithIndex().map(_.swap).collectAsMap())
 
-
     println("RESULTADO: ")
-    model.printSchema()
+    model.printSchema(";")
 
-
+    sc.parallelize(model.saveSchema).saveAsTextFile(destino + "Linkage-" + destino)
 
     sc.stop()
   }
