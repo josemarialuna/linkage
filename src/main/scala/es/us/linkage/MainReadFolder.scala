2763d82f5b4f4234546ae4fc51165986e3d5b324
José María Luna <josemarialunaromera@gmail.com>
2018-01-19 12:17:44 +0100

    Last commit

--- src/main/scala/es/us/linkage/MainReadFolder.scala
+++ src/main/scala/es/us/linkage/MainReadFolder.scala
@@ -1,5 +1,6 @@
 package es.us.linkage
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object MainReadFolder {
@@ -16,13 +17,12 @@ object MainReadFolder {
 
     sc.setCheckpointDir("checkpoints")
 
-    val fileOriginal = "C:\\datasets\\distancesMap"
-    val fileTest = "C:\\datasets\\distanceTest"
+    val fileTest = "src/main/resources/distanceTest"
 
     var origen: String = fileTest
     var destino: String = Utils.whatTimeIsIt()
     var numPartitions = 8 // cluster has 25 nodes with 4 cores. You therefore need 4 x 25 = 100 partitions.
-    var numPoints = 7
+    var numPoints = 9
     var numClusters = 1
     var strategyDistance = "min"
 
@@ -44,12 +44,33 @@ object MainReadFolder {
         new Distance(x(0).toInt, x(1).toInt, x(2))
       }.filter(x => x.getIdW1 < x.getIdW2)
 
-    //val distances: RDD[Double] = sc.parallelize(Seq(
-    //(0.0), (1.0), (3.0), (5.0), (6.0), (1.0), (0.0), (3.0), (7.0), (8.0), (3.0), (3.0), (0.0), (7.0), (8.0), (5.0), (7.0), (7.0), (0.0), (2.0), (6.0), (8.0), (8.0), (2.0), (0.0)) )
+//    val distances: RDD[Distance] = sc.parallelize(Seq(
+//      new Distance(1,2,2),
+//      new Distance(1,3,6),
+//      new Distance(1,4,50),
+//      new Distance(1,5,50),
+//      new Distance(1,6,50),
+//      new Distance(1,7,50),
+//      new Distance(2,3,50),
+//      new Distance(2,4,50),
+//      new Distance(2,5,8),
+//      new Distance(2,6,11),
+//      new Distance(2,7,50),
+//      new Distance(3,4,4),
+//      new Distance(3,5,50),
+//      new Distance(3,6,50),
+//      new Distance(3,7,50),
+//      new Distance(4,5,50),
+//      new Distance(4,6,50),
+//      new Distance(4,7,12),
+//      new Distance(5,6,50),
+//      new Distance(5,7,50),
+//      new Distance(6,7,10)
+//    ))
 
 
-    //val data = sc.parallelize(Cluster.createInitClusters(numPoints))
-    //println(data.count())
+    val data = sc.parallelize(Cluster.createInitClusters(numPoints))
+    println(data.count())
 
     //min,max,avg
     val linkage = new Linkage(numClusters, strategyDistance)
